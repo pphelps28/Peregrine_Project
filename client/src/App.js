136f5f647ab1413d9c484ac20a0afb5922bae8f7
Max-Costes <mxcostes@gmail.com>
2020-04-02 21:53:45 -0400

    near complete, needs work on none text input value capture

--- client/src/App.js
+++ client/src/App.js
@@ -4,42 +4,120 @@ import React, { Component } from 'react'
 import 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import InputForm from './components/InputForm'
-import Display from './components/Display'
+
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       name: '',
-      location: '',
+      email: '',
+      bird: '',
+      site: ['one', 'two'],
       date_observed: new Date(),
-      notes: '',
-      inputVisible: true,
-      displayContent: []
+      mileage: '',
+      travel: '',
+      timeStart: '',
+      timeEnd: '',
+      totalTime: '',
+      temperature: '',
+      percipitation: '',
+      cloudCover: '',
+      windSpeed: '',
+      observationSummary: [],
+      incubation: '',
+      young: '',
+      youngAge: '',
+      observation: '',
+      comments: ''
     }
   }
   nameChange = (event) => {
     this.setState({ name: event.target.value })
   }
-  locationChange = (event) => {
+  emailChange = (event) => {
+    this.setState({ name: event.target.value })
+  }
+  siteChange = (event) => {
 
-    this.setState({ location: event.target.value })
+    this.setState({ site: event.target.value })
+  }
+  birdChange = (event) => {
+    this.setState({ bird: event.target.value })
   }
   dateChange = date => {
 
     this.setState({ date_observed: date })
   }
-  notesChange = (event) => {
-    this.setState({ notes: event.target.value })
+  mileageChange = (event) => {
+    this.setState({ mileage: event.target.value })
+  }
+  travelChange = (event) => {
+    this.setState({ travel: event.target.value })
+  }
+  timeStartChange = (event) => {
+    this.setState({ timeStart: event.target.value })
+  }
+  timeEndChange = (event) => {
+    this.setState({ timeEnd: event.target.value })
+  }
+  totalTimeChange = (event) => {
+    this.setState({ totalTime: event.target.value })
+  }
+  temperatureChange = (event) => {
+    this.setState({ temperature: event.target.value })
+  }
+  percipitationChange = (event) => {
+    this.setState({ percipitation: event.target.value })
+  }
+  cloudCoverChange = (event) => {
+    this.setState({ cloudCover: event.target.value })
+  }
+  windSpeedChange = (event) => {
+    this.setState({ windSpeed: event.target.value })
+  }
+  observationSummaryChange = (event) => {
+    this.setState({ observationSummary: event.target.value })
+  }
+  youngChange = (event) => {
+    this.setState({ young: event.target.value })
+  }
+  incubationChange = (event) => {
+    this.setState({ incubation: event.target.value })
+  }
+  youngAgeChange = (event) => {
+    this.setState({ youngAge: event.target.value })
+  }
+  observationChange = (event) => {
+    this.setState({ observation: event.target.value })
+  }
+  commentsChange = (event) => {
+    this.setState({ comments: event.target.value })
   }
 
   handleSubmit = (event) => {
     event.preventDefault()
     let submission = {
       name: this.state.name,
-      location: this.state.location,
-      date_observed: this.state.date_observed,
-      date_submitted: new Date(),
-      notes: this.state.notes
+      email: this.state.email,
+      bird: this.state.bird,
+      site: this.state.site,
+      date_observed: new Date(),
+      mileage: this.state.mileage,
+      travel: this.state.travel,
+      timeStart: this.state.timeStart,
+      timeEnd: this.state.timeEnd,
+      totalTime: this.state.totalTime,
+      temperature: this.state.temperature,
+      percipitation: this.state.percipitation,
+      cloudCover: this.state.cloudCover,
+      windSpeed: this.state.windSpeed,
+      observationSummary: this.state.observationSummary,
+      incubation: this.state.incubation,
+      young: this.state.young,
+      youngAge: this.state.youngAge,
+      observation: this.state.observation,
+      comments: this.state.comment,
     }
     fetch('/post', {
       method: 'POST',
@@ -50,37 +128,48 @@ class App extends Component {
     })
     this.setState({
       name: '',
-      location: '',
-      date_observed: '',
-      notes: ''
+      email: '',
+      bird: '',
+      site: ['one', 'two'],
+      date_observed: new Date(),
+      mileage: '',
+      travel: '',
+      timeStart: '',
+      timeEnd: '',
+      totalTime: '',
+      temperature: '',
+      percipitation: '',
+      cloudCover: '',
+      windSpeed: '',
+      observationSummary: [],
+      incubation: '',
+      young: '',
+      youngAge: '',
+      observation: '',
+      comments: '',
     })
   }
 
-  toggleInput = () => {
-    if (this.state.inputVisible) {
-      fetch('/display').then(res => {
-        return res.json()
-      }).then(jsonObj => {
-        this.setState({ displayContent: jsonObj })
-      })
-    }
-    this.state.inputVisible ? this.setState({ inputVisible: false }) : this.setState({ inputVisible: true })
-  }
+  
 
   render() {
-    let { name, location, date_observed, notes, displayContent } = this.state
-    let { nameChange, locationChange, dateChange, notesChange, handleSubmit, toggleInput } = this
+    let { name, email, bird, site,  date_observed, mileage, travel, timeStart,timeEnd, totalTime, temperature, percipitation, cloudCover, windSpeed, observationSummary, young, youngAge, incubation, observation, comments} = this.state
+    let { nameChange, emailChange, birdChange, siteChange, dateChange, mileageChange,travelChange, timeStartChange, timeEndChange, totalTimeChange, temperatureChange, percipitationChange,cloudCoverChange, windSpeedChange, observationChange,observationSummaryChange, youngChange, youngAgeChange, incubationChange, commentsChange, handleSubmit, toggleInput } = this
     return (
       <>
         <button id="toggle-button" onClick={toggleInput}>Toggle</button>
         <div id="wrapper">
-          {this.state.inputVisible ?
+          
             <InputForm handleSubmit={handleSubmit}
-              name={name} location={location}
-              date_observed={date_observed} notes={notes}
-              nameChange={nameChange} locationChange={locationChange}
-              dateChange={dateChange} notesChange={notesChange} />
-            : <Display displayContent={displayContent} />}
+              name={name} email={email} bird={bird} site={site} 
+              date_observed={date_observed} mileage={mileage} travel={travel} timeStart={timeStart} timeEnd={timeEnd} totalTime={totalTime} temperature={temperature} percipitation={percipitation} 
+              cloudCover={cloudCover} windSpeed={windSpeed} observationSummary={observationSummary} young={young} youngAge={youngAge} incubation={incubation} observation={observation} comments={comments}
+              nameChange={nameChange} emailChange={emailChange} birdChange={birdChange} siteChange={siteChange} dateChange={dateChange} mileageChange={mileageChange} travelChange={travelChange}
+               timeStartChange={timeStartChange} timeEndChange={timeEndChange} totalTimeChange={totalTimeChange} temperatureChange={temperatureChange} percipitationChange={percipitationChange} 
+               cloudCoverChange={cloudCoverChange} windSpeedChange={windSpeedChange} observationChange={observationChange} observationSummaryChange={observationSummaryChange} 
+               youngChange={youngChange} youngAgeChange={youngAgeChange} incubationChange={incubationChange} commentsChange={commentsChange} handleSubmit={handleSubmit} 
+              />
+           
         </div>
       </>
     )
--- client/src/components/InputForm.js
+++ client/src/components/InputForm.js
@@ -1,72 +1,79 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
+import { Form, Row, Col } from 'react-bootstrap';
 
 export default function InputForm(props) {
 	return (
 		<div>
-			{/*------------------------- Insert name ----------------------*/}
-			<div className="form-group"> 
-          <label>Name </label>
-          <input  type="text"
-              required
-              className="form-control"
-              value={props.name}
-              onChange={props.nameChange}
-              />
-        </div>
+			<Form>
+				{/*------------------------- Insert name ----------------------*/}
+				<div className="form-group">
+					<label>Name </label>
+					<input
+						type="text"
+						required
+						className="form-control"
+						value={props.name}
+						onChange={props.nameChange}
+					/>
+				</div>
 				{/*------------------------- email  -----------------------*/}
-				<div className="form-group"> 
-          <label>Email </label>
-          <input  type="text"
-              required
-              className="form-control"
-              value={props.email}
-              onChange={props.emialChange}
-              />
-        </div>
+				<div className="form-group">
+					<label>Email </label>
+					<input
+						type="text"
+						required
+						className="form-control"
+						value={props.email}
+						onChange={props.emailChange}
+					/>
+				</div>
 				{/* -----------------------Bird Observed Radio -------------------*/}
-				<Form.Group as={Row}>
-					<Form.Label as="legend" column sm={2}>
+				<Form.Group as={Col}>
+					<Form.Label as="legend" column sm={10}>
 						Which Bird did you Observe
 					</Form.Label>
 					<Col sm={10}>
 						<Form.Check
 							type="radio"
 							label="Bald Eagle"
+							value="Bald Eagle"
 							name="formHorizontalRadios"
 							id="formHorizontalRadios1"
 						/>
 						<Form.Check
 							type="radio"
 							label="Peregrine Falcon"
+							value="Peregrine Falcon"
 							name="formHorizontalRadios"
 							id="formHorizontalRadios2"
 						/>
 					</Col>
 				</Form.Group>
 				{/*------------------------- Location Site Name ------------------------*/}
-				<div className="form-group"> 
-          <label>Select Site </label>
-          <select ref="userInput"
-              required
-              className="form-control"
-              value={props.site}
-              onChange={props.onChangeSite}>
-              {
-                props.site.map((user)=> {
-                  return <option 
-                    key={site}
-                    value={site}>{site}
-                    </option>;
-                })
-              }
-          </select>
-        </div>
+				<div className="form-group">
+					<label>Select Site </label>
+					<select
+						
+						required
+						className="form-control"
+						value={props.site}
+						onChange={props.siteChange}
+					>
+						{props.site.map((site) => {
+							return (
+								<option key={site} value={site}>
+									{site}
+								</option>
+							);
+						})}
+					</select>
+				</div>
 				{/*-------------------------- Date Observed -------------------------------*/}
 				<div className="form-group">
 					<label>Date: </label>
 					<div>
-						<DatePicker selected={this.state.date_observed} onChange={props.dateChange} />
+						<DatePicker selected={props.date_observed} onChange={props.dateChange} />
 					</div>
 				</div>
 				{/* ---------------------------- Mileage to and from site -------------------*/}
@@ -76,8 +83,8 @@ export default function InputForm(props) {
 						type="text"
 						required
 						className="form-control"
-						value={this.state.mileage}
-						onChange={this.onChangeMileage}
+						value={props.mileage}
+						onChange={props.mileageChange}
 					/>
 				</div>
 				{/* ------------------------------------------ Travel Time ------------------------------*/}
@@ -87,101 +94,94 @@ export default function InputForm(props) {
 						type="text"
 						required
 						className="form-control"
-						value={this.state.travel}
-						onChange={this.onChangeTravel}
+						value={props.travel}
+						onChange={props.travelChange}
 					/>
 				</div>
 				{/* --------------------------------------- Observation Start Time ------------------------------*/}
-                <div className="form-group">
+				<div className="form-group">
 					<label>Observation Start Time: </label>
 					<div>
-				<DatePicker selected={props.timeStart} onChange={this.handleTimeStart} showTimeSelect dateFormat="Pp" />
-                </div>
+						<DatePicker
+							selected={props.timeStart}
+							onChange={props.timeStartChange}
+							showTimeSelect
+							dateFormat="Pp"
+						/>
+					</div>
 				</div>
-                {/* -------------------------------------- Observation End Time -------------------------- */}
-                <div className="form-group">
+				{/* -------------------------------------- Observation End Time -------------------------- */}
+				<div className="form-group">
 					<label>Observation End Time: </label>
 					<div>
-				<DatePicker selected={props.timeEnd} onChange={props.handleTimeEnd} showTimeSelect dateFormat="Pp" />
-                </div>
+						<DatePicker
+							selected={props.timeEnd}
+							onChange={props.timeEndChange}
+							showTimeSelect
+							dateFormat="Pp"
+						/>
+					</div>
 				</div>
-            {/* ----------------------------------- Total Observation Time --------------------------- */}
-            <div className="form-group">
+				{/* ----------------------------------- Total Observation Time --------------------------- */}
+				<div className="form-group">
 					<label>Total Observation Time: </label>
 					<input
 						type="text"
 						required
 						className="form-control"
-						value={this.state.totalTime}
-						onChange={this.onChangeTotalObservationTime}
-                        />
+						value={props.totalTime}
+						onChange={props.totalTimeChange}
+					/>
 				</div>
-                {/* ---------------------------- Temperature --------------------------*/}
-                <div className="form-group">
+				{/* ---------------------------- Temperature --------------------------*/}
+				<div className="form-group">
 					<label>Temperature (in F, can provide a 5-10 degree range): </label>
 					<input
 						type="text"
 						required
 						className="form-control"
 						value={props.temperature}
-						onChange={props.onChangeTemperature}
-                        />
+						onChange={props.temperatureChange}
+					/>
 				</div>
-                {/* ------------------------- Percipitation ---------------------------------*/}
-                <Form.Group as={Row}>
-					<Form.Label as="legend" column sm={2}>
+				{/* ------------------------- Percipitation ---------------------------------*/}
+				<Form.Group as={Col}>
+					<Form.Label as="legend" column sm={10}>
 						Percipitation
 					</Form.Label>
 					<Col sm={10}>
-						<Form.Check
-							type="radio"
-							label="Fog"
-							name="formHorizontalRadios"
-							id="formHorizontalRadios1"
-						/>
+						<Form.Check type="radio" label="Fog" name="formHorizontalRadios" id="formHorizontalRadios1" />
 						<Form.Check
 							type="radio"
 							label="Drizzle"
 							name="formHorizontalRadios"
-							id="formHorizontalRadios2"
+							id="formHorizontalRadios3"
 						/>
-                        <Form.Check
+						<Form.Check
 							type="radio"
 							label="Rain-heavy"
 							name="formHorizontalRadios"
-							id="formHorizontalRadios2"
+							id="formHorizontalRadios4"
 						/>
-                        <Form.Check
+						<Form.Check
 							type="radio"
 							label="Rain-light"
 							name="formHorizontalRadios"
-							id="formHorizontalRadios2"
+							id="formHorizontalRadios5"
 						/>
-                        <Form.Check
-							type="radio"
-							label="None"
-							name="formHorizontalRadios"
-							id="formHorizontalRadios2"
-						/>
-                        <Form.Check
-							type="radio"
-							label="Other"
-							name="formHorizontalRadios"
-							id="formHorizontalRadios2"
-						/>
-					</Col>
+						</Col>
 				</Form.Group>
-                {/* ------------------------------Cloud Cover---------------- */}
-                <Form.Group as={Row}>
-					<Form.Label as="legend" column sm={2}>
-						Percipitation
+				{/* ------------------------------Cloud Cover---------------- */}
+				<Form.Group as={Col}>
+					<Form.Label as="legend" column sm={10}>
+						Cloud Cover
 					</Form.Label>
 					<Col sm={10}>
 						<Form.Check
 							type="radio"
 							label="Clear =<10% cover"
 							name="formHorizontalRadios"
-							id="formHorizontalRadios1"
+							id="formHorizontalRadios7"
 						/>
 						<Form.Check
 							type="radio"
@@ -189,13 +189,13 @@ export default function InputForm(props) {
 							name="formHorizontalRadios"
 							id="formHorizontalRadios2"
 						/>
-                        <Form.Check
+						<Form.Check
 							type="radio"
 							label="Broken = 50-90% cover"
 							name="formHorizontalRadios"
 							id="formHorizontalRadios2"
 						/>
-                        <Form.Check
+						<Form.Check
 							type="radio"
 							label="Overcast = >90% cover"
 							name="formHorizontalRadios"
@@ -203,10 +203,10 @@ export default function InputForm(props) {
 						/>
 					</Col>
 				</Form.Group>
-                {/* ---------------------------Wind Speed ---------------------- */}
-                <Form.Group as={Row}>
-					<Form.Label as="legend" column sm={2}>
-						Percipitation
+				{/* ---------------------------Wind Speed ---------------------- */}
+				<Form.Group as={Col}>
+					<Form.Label as="legend" column sm={10}>
+						Wind Speed
 					</Form.Label>
 					<Col sm={10}>
 						<Form.Check
@@ -221,19 +221,19 @@ export default function InputForm(props) {
 							name="formHorizontalRadios"
 							id="formHorizontalRadios2"
 						/>
-                        <Form.Check
+						<Form.Check
 							type="radio"
 							label="10-15 mph = strong"
 							name="formHorizontalRadios"
 							id="formHorizontalRadios2"
 						/>
-                        <Form.Check
+						<Form.Check
 							type="radio"
 							label="15-20 mph = very strong"
 							name="formHorizontalRadios"
 							id="formHorizontalRadios2"
 						/>
-                        <Form.Check
+						<Form.Check
 							type="radio"
 							label=">20 mph not recommended"
 							name="formHorizontalRadios"
@@ -241,9 +241,9 @@ export default function InputForm(props) {
 						/>
 					</Col>
 				</Form.Group>
-                {/* ---------------------------------Observation Summary ----------------------- */}
-                <Form.Group as={Row}>
-					<Form.Label as="legend" column sm={2}>
+				{/* ---------------------------------Observation Summary ----------------------- */}
+				<Form.Group as={Col}>
+					<Form.Label as="legend" column sm={10}>
 						Observation Summary (all that apply)
 					</Form.Label>
 					<Col sm={10}>
@@ -259,37 +259,37 @@ export default function InputForm(props) {
 							name="formHorizontalRadios"
 							id="formHorizontalRadios2"
 						/>
-                        <Form.Check
+						<Form.Check
 							type="checkbox"
 							label="Courtship/Copulation"
 							name="formHorizontalRadios"
 							id="formHorizontalRadios2"
 						/>
-                        <Form.Check
+						<Form.Check
 							type="checkbox"
 							label="Incubating"
 							name="formHorizontalRadios"
 							id="formHorizontalRadios2"
 						/>
-                        <Form.Check
+						<Form.Check
 							type="checkbox"
 							label="Hatched"
 							name="formHorizontalRadios"
 							id="formHorizontalRadios2"
 						/>
-                        <Form.Check
+						<Form.Check
 							type="checkbox"
 							label="Nest Failure"
 							name="formHorizontalRadios"
 							id="formHorizontalRadios2"
 						/>
-                        <Form.Check
+						<Form.Check
 							type="checkbox"
 							label="Human Disturbance"
 							name="formHorizontalRadios"
 							id="formHorizontalRadios2"
 						/>
-                        <Form.Check
+						<Form.Check
 							type="checkbox"
 							label="Fledged"
 							name="formHorizontalRadios"
@@ -297,68 +297,70 @@ export default function InputForm(props) {
 						/>
 					</Col>
 				</Form.Group>
-                {/* -----------------------------Incubation -------------------------- */}
-                <div className="form-group">
+				{/* -----------------------------Incubation -------------------------- */}
+				<div className="form-group">
 					<label>If incubation was confirmed, where is the location of the eyrie? </label>
 					<input
 						type="text"
 						required
 						className="form-control"
 						value={props.incubation}
-						onChange={props.onChangeIncubation}
-                        />
+						onChange={props.incubationChange}
+					/>
 				</div>
-                {/* -------------------------------Young ----------------- */}
-                <div className="form-group">
+				{/* -------------------------------Young ----------------- */}
+				<div className="form-group">
 					<label>If young exist, how many were there? </label>
 					<input
 						type="text"
 						required
 						className="form-control"
 						value={props.young}
-						onChange={props.onChangeYoung}
-                        />
+						onChange={props.youngChange}
+					/>
 				</div>
-                {/* ---------------------------Young ages ---------------------- */}
-                <div className="form-group">
+				{/* ---------------------------Young ages ---------------------- */}
+				<div className="form-group">
 					<label>If young exist, what were the aproximate ages? </label>
 					<input
 						type="text"
 						required
 						className="form-control"
 						value={props.youngAge}
-						onChange={props.onChangeYoungAges}
-                        />
+						onChange={props.youngAgeChange}
+					/>
 				</div>
-                {/* -----------------------------Recorded Observations ------------------- */}
-                <div className="form-group"> 
-          <label>'Recorded Observations (include time of observation)' </label>
-          <textarea  type="text"
-              required
-              className="form-control"
-              value={props.observation}
-              onChange={props.onChangeObservation}
-              rows= '6'
-              />
-        </div>
-        {/* ---------------------Remarks Comments --------------------------*/}
-        <div className="form-group"> 
-          <label>Remarks/Comments </label>
-          <textarea  type="text"
-              required
-              className="form-control"
-              value={props.comments}
-              onChange={props.onChangeComments}
-              rows= '6'
-              />
-        </div>
-
-                        </Form>
+				{/* -----------------------------Recorded Observations ------------------- */}
+				<div className="form-group">
+					<label>'Recorded Observations (include time of observation)' </label>
+					<textarea
+						type="text"
+						required
+						className="form-control"
+						value={props.observation}
+						onChange={props.observationChange}
+						rows="6"
+					/>
+				</div>
+				{/* ---------------------Remarks Comments --------------------------*/}
+				<div className="form-group">
+					<label>Remarks/Comments </label>
+					<textarea
+						type="text"
+						required
+						className="form-control"
+						value={props.comments}
+						onChange={props.commentsChange}
+						rows="6"
+					/>
+				</div>
+			</Form>
 		</div>
 	);
 }
 
-<div id="container">
+{
+	/* <div id="container">
 	<form id="form-input" onSubmit={props.handleSubmit}>
 		Name <input className="input" type="text" name="name" value={props.name} onChange={props.nameChange} />
 		Location{' '}
@@ -382,4 +384,5 @@ export default function InputForm(props) {
 		/>
 		<input className="input" type="submit" />
 	</form>
-</div>;
+</div>; */
+}
