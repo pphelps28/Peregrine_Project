74f59f611b0b892a707019c91ecbd8341c886295
Max-Costes <mxcostes@gmail.com>
2020-04-05 20:16:00 -0400

    added responsiveness

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
-import {Jumbotron, Container} from 'react-bootstrap'
+import {Row, Col, Container, Image} from 'react-bootstrap'
 
 export class Header extends Component {
     render() {
         return (
-        <Jumbotron fluid>
+     
             <Container>
             <div className='Header'>
-                <img className='headerImages' src='../../img/Audubon_H_BLK_VT.png' alt='Audobon Logo' fluid='true' ></img>
-                <img className='headerImages' src="../../img/VFWDlogo.jpg" alt='Fish and Wildlife Logo' fluid='true'></img>
+                <Row className = 'imageContainer'>
+                <Col xs={12}  md={6}>
+                <Image  className='headerImages' src='../../img/Audubon_H_BLK_VT.png' alt='Audobon Logo' fluid='true'></Image>
+                </Col>
+                <Col  xs={12}  md={6}>
+                <Image className='headerImages' src="../../img/VFWDlogo.jpg" alt='Fish and Wildlife Logo' fluid='true'></Image>
+                </Col>
+                </Row>
             </div>
             </Container>
-          </Jumbotron>
+         
            
         )
     }
