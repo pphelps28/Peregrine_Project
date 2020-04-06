3a9ca31e3c78c72c1285fd108d7ec4e80b7b6fe9
Max-Costes <mxcostes@gmail.com>
2020-04-06 13:11:31 -0400

    navbar in progress

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import {Navbar, Nav, Button, Form, FormControl} from 'react-bootstrap'
-import ModalLogIn from '../components/ModalLogIn'
+import { Navbar, Nav, Button, Form, FormControl } from 'react-bootstrap';
+import ModalLogIn from '../components/ModalLogIn';
 
 export class NavBar extends Component {
-    constructor(props) {
-        super(props)
+	constructor(props) {
+		super(props);
 
-        this.state= {
-            inputVisible: true
-        }
-    }
+		this.state = {
+			inputVisible: true
+		};
+	}
 	render() {
 		return (
 			<div>
@@ -18,12 +18,10 @@ export class NavBar extends Component {
 					<Nav className="mr-auto">
 						<Nav.Link href="#home">Submit Forms</Nav.Link>
 						<Nav.Link onClick={this.props.toggleInput}>Observation Report</Nav.Link>
-						<Nav.Link><ModalLogIn></ModalLogIn></Nav.Link>
+						<Nav.Link>
+							<ModalLogIn />
+						</Nav.Link>
 					</Nav>
-					<Form inline>
-						<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-						<Button variant="outline-light">Search</Button>
-					</Form>
 				</Navbar>
 			</div>
 		);
