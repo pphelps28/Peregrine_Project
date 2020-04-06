27da787d00640c5289d5e6fce219c05746eee07b
pphelps28 <p.w.phelps28@gmail.com>
2020-04-06 17:00:20 -0400

    Styling Changes

--- client/src/components/ModalLogIn.js
+++ client/src/components/ModalLogIn.js
@@ -1,48 +1,48 @@
 import React, { Component } from 'react'
-import {Modal, Button} from 'react-bootstrap'
+import { Modal, Button } from 'react-bootstrap'
 import LoginModal from '../components/LoginModal'
-
+import { Link } from 'react-router-dom'
 
 function MyVerticallyCenteredModal(props) {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-           Log In 
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Log In
           </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <LoginModal />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-  
-  function ModalLogIn() {
-    const [modalShow, setModalShow] = React.useState(true);
-  
-    return (
-      <>
-        <Button variant="primary" onClick={() => setModalShow(true)}>
-          Log In Modal
-        </Button>
-      
-        <MyVerticallyCenteredModal
-          show={modalShow}
-          onHide={() => setModalShow(false)}
-        />
-      </>
-    );
-  }
-  
+      </Modal.Header>
+      <Modal.Body>
+        <LoginModal />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function ModalLogIn() {
+  const [modalShow, setModalShow] = React.useState(true);
+
+  return (
+    <>
+      <Link className="link" variant="primary" onClick={() => setModalShow(true)}>
+        Log In
+        </Link>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+}
 
 
-  export default ModalLogIn
+
+export default ModalLogIn
