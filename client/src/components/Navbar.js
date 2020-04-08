dcf77885bd23296306c0aa3e3f6297e082fa5cbd
Brook Martenis <jbegonya@protonmail.com>
2020-04-08 07:28:22 -0400

    trimming VFW logo, inverting audubon logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,7 +15,7 @@ export class NavBar extends Component {
 		return (
 			<div>
 				<Navbar fixed="top" bg="primary" variant="dark" className='navContainer'>
-					<Navbar.Brand><Link to='/' className='link'><img className='brandImage' src='../../img/Audubon_H_BLK_VT.png'></img></Link></Navbar.Brand>
+					<Navbar.Brand><Link to='/' className='link'><img className='brandImage' src='../../img/Audubon_H_WHT_VT.png'></img></Link></Navbar.Brand>
 					<Nav className="mr-auto">
 						<Nav.Link ><Link to='/' className='link'>Observation Form</Link></Nav.Link>
 						<Nav.Link onClick={this.props.toggleInput}><Link to="/display" className='link'>View Reports</Link></Nav.Link>
