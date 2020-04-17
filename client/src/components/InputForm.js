46fc25b4cb6ad517a938c154ef2b3fd240329eaf
robmitch802 <60521912+robmitch802@users.noreply.github.com>
2020-04-17 11:28:41 -0400

    timepicker and form submit fix

--- client/src/components/InputForm.js
+++ client/src/components/InputForm.js
@@ -11,7 +11,7 @@ export default function InputForm(props) {
 	return (
 		<div className="form">
 			<Header />
-			<Form className="d-xs-block d-sm-block d-md-block d-lg-block d-xl-block">
+			<Form className="d-xs-block d-sm-block d-md-block d-lg-block d-xl-block" onSubmit={props.handleSubmit}>
 				<h3 className="title">Observation Form</h3>
 				{/*------------------------- Insert name ----------------------*/}
 				<div className="form-group">
@@ -112,14 +112,14 @@ export default function InputForm(props) {
 				<div className="form-group">
 					<label>Start Time of Observation:</label>
 					<br></br>
-					<TimePicker amPmAriaLabel disableClock={true} clearIcon selected={props.timeStart} onChange={props.timeStartChange} />
+					<TimePicker amPmAriaLabel={"amPmAriaLabel"} disableClock={true} clearIcon={null} selected={props.timeStart} onChange={props.timeStartChange} />
 				</div>
 
 				{/* -------------------------------------- Observation End Time -------------------------- */}
 				<div className="form-group">
 					<label>End Time of Observation:</label>
 					<br></br>
-					<TimePicker amPmAriaLabel disableClock={true} clearIcon selected={props.timeEnd} onChange={props.timeEndChange} />
+					<TimePicker amPmAriaLabel={"amPmAriaLabel"} disableClock={true} clearIcon={null} selected={props.timeEnd} onChange={props.timeEndChange} />
 				</div>
 
 				{/* ----------------------------------- Total Observation Time --------------------------- */}
@@ -604,7 +604,7 @@ export default function InputForm(props) {
 					/>
 				</div>
 				{/* --------------------------Submit-------------------------------- */}
-				<input type="submit" value="Submit Form" className="btn btn-primary" onClick={props.handleSubmit} />
+				<button type="submit" value="Submit Form" className="btn btn-primary" >Submit Form</button>
 			</Form>
 		</div >
 	);
