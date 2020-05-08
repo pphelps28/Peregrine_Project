c5aa422bfa9825e3d22709a231be8cefd9f8a484
pphelps28 <p.w.phelps28@gmail.com>
2020-05-08 14:21:44 -0400

    Firebase auth transfer

--- client/src/App.js
+++ client/src/App.js
@@ -14,14 +14,13 @@ import ReportModal from './components/ReportModal'
 
 //firebase => .env
 const firebaseConfig = {
-  apiKey: "AIzaSyAio6vwdZAJ1GlzX7C0Mg8bR6gLt1EpVBQ",
-  authDomain: "fir-practice-87288.firebaseapp.com",
-  databaseURL: "https://fir-practice-87288.firebaseio.com",
-  projectId: "fir-practice-87288",
-  storageBucket: "fir-practice-87288.appspot.com",
-  messagingSenderId: "270975170469",
-  appId: "1:270975170469:web:cef00b31317d4d81b61a4c",
-  measurementId: "G-R28BVC59V8"
+  apiKey: "AIzaSyAanCsZeWqOpUrMGowYBCtfQA43h8t1QPI",
+  authDomain: "audubon-form.firebaseapp.com",
+  databaseURL: "https://audubon-form.firebaseio.com",
+  projectId: "audubon-form",
+  storageBucket: "audubon-form.appspot.com",
+  messagingSenderId: "681694453721",
+  appId: "1:681694453721:web:8e3db9d26c458fc2da83c8"
 };
 firebase.initializeApp(firebaseConfig)
 const auth = firebase.auth()
